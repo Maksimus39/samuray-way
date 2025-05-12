66f5128d13410aa2fdb83f13d1c7da69d2d3c8a3
Max <maksim.minakov.83@mail.ru>
2025-05-12 08:11:19 +0300

    lesson-20

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import styles from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <div className={styles.list}>
-            <a href="/profile">Profile</a>
-            <a href="/dialogs">Messages</a>
-            <a href="/news">News</a>
-            <a href="/music">Music</a>
-            <a href="/settings">Settings</a>
-        </div>
+        <nav className={styles.list}>
+            <div className={styles.item}>
+                <NavLink to="/profile">Profile</NavLink>
+            </div>
+            <div className={styles.item}>
+                <NavLink to="/dialogs">Messages</NavLink>
+            </div>
+            <div className={styles.item}>
+                <NavLink to="/news">News</NavLink>
+            </div>
+            <div className={styles.item}>
+                <NavLink to="/music">Music</NavLink>
+            </div>
+            <div className={styles.item}>
+                <NavLink to="/settings">Settings</NavLink>
+            </div>
+        </nav>
     );
 };
